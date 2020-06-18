6c9c854c70dd2f92bbbe6157dea7a22b4dbb587f
Makuto <derickmakuto@outlook.com>
2020-06-18 19:02:59 +0300

    first commit

---
+++ src/components/Display.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const style = {
+    fontSize: 110,
+    color: 'lightgrey',
+    position: 'absolute',
+    top: '30%'
+};
+
+export const Display = (props) => {
+    return (
+        <div style={style}>{props.number}</div>
+    )
+} 
+
